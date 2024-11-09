13bc831e37f14ea8684ab2e93c7a0eed978e8ed3
VoltagedDebunked <rusindanilo@gmail.com>
2024-11-09 23:29:01 +0200

    Make it a base for now

--- src/kernel.c
+++ src/kernel.c
@@ -1,11 +1,6 @@
 #include "kernel.h"
-#include "drivers/keyboard.h"
 
 void kernel_main() {
-    kernel_init();
-    keyboard_init();
-
-    // Kernel loop
     while(1) {
     }
 }
--- src/kernel.h
+++ src/kernel.h
@@ -1,8 +1,6 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
-// Kernel initialization functions
-void kernel_init();
 void kernel_main();
 
 #endif // KERNEL_H
